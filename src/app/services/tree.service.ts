d8986d6e570bcdb06fa7fc32a25dfc95b4e30ec7
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2022-09-24 09:51:14 +0200

    add: tree display

---
+++ src/app/services/tree.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Tree } from '../model/tree';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TreeService {
+  private readonly url = `${environment.apiUrl}/tree`;
+  constructor(private readonly http: HttpClient) {}
+
+  getAll$(): Observable<Tree[]> {
+    return this.http.get<Tree[]>(this.url + '/all');
+  }
+}
