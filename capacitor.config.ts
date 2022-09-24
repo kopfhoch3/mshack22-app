bde7ef66736386918d659d02e79bbbb6a034759b
Niklas Schoellhorn <schoellhorn.niklas@gmail.com>
2022-09-24 12:41:09 +0200

    Add iOS capabilities for demo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'de.kopf3.mshack',
   appName: 'mshack-22-app',
   webDir: 'www',
   bundledWebRuntime: false
